1431cc56d43fff2bedcf68e0e63a7daa5475e411
Ashley  Zaki <ahnahn.un@gmail.com>
2018-10-22 23:32:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,6 +24,7 @@ end
 def find_the_cheese(list)
   cheese_types = ["cheddar", "gouda", "camembert"]
   if list.include?(cheese_types)
-    return list
+    return cheese_types
   end
+  puts list
 end
