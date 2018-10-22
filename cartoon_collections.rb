73f026b2fc4ed5226d921dbd00a79fe644e834db
Ashley  Zaki <ahnahn.un@gmail.com>
2018-10-22 23:04:04 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,8 @@ def summon_captain_planet(summon)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(word)
+  word.any?
 end
 
 def find_the_cheese# code an argument here
